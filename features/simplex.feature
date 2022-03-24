9b04b353af2c8eb9c3c9c0bf1979433d159b3af7
Andrey <pavlenkoandrey56@gmail.com>
2022-03-24 23:51:30 +0200

    added cases of simplex deposit

--- features/simplex.feature
+++ features/simplex.feature
@@ -1,9 +1,32 @@
 Feature: Simplex
 
-    @test
     Scenario Outline: Make a deposit by simplex
         Given User try to make a deposit from <from_asset> to <to_asset> with amount <amount>
         Examples:
             | from_asset | to_asset   | amount |
             | USD        | ETH        | 110    |
             | USD        | XLM        | 110    |
+    
+    Scenario Outline: Make a deposit by simplex with non exists asset from
+        Given User try to make a deposit by simplex with asset to <to_asset>, amount <amount>,  non exists asset from
+        Examples:
+            | to_asset   | amount |
+            | ETH        | 110    |
+    
+    Scenario Outline: Make a deposit by simplex with non exists asset to
+        Given User try to make a deposit by simplex with asset from <from_asset>, amount <amount>,  non exists asset to
+        Examples:
+            | from_asset | amount |
+            | USD        | 110    |
+    
+    Scenario Outline: Make a deposit by simplex with amount less than minimum
+        Given User try to make a deposit by simplex with asset to <to_asset>, asset from <from_asset> and amount <amount>
+        Examples:
+            | from_asset | to_asset | amount |
+            | USD        | XLM      |    1   |
+       
+    Scenario Outline: Make a deposit by simplex with amount more than maximum
+        Given User try to make a deposit by simplex with asset to <to_asset>, asset from <from_asset> and amount more than maximum <amount>
+        Examples:
+            | from_asset | to_asset | amount   |
+            | USD        | XLM      | 10100101 |
--- tests/test_simplex.py
+++ tests/test_simplex.py
@@ -10,7 +10,54 @@ def test_simplex_deposit():
 @given(parsers.parse('User try to make a deposit from {from_asset} to {to_asset} with amount {amount}'))
 def create_payment(from_asset, to_asset, amount, auth):
     token = auth(settings.me_tests_email, settings.me_tests_password )
-    print(token)
     result = Simplex().create_payment(token, from_asset, to_asset, amount)
     assert type(result) == dict,  f"Expected: result will be dict type but returned: {result}"
     assert result['result'] == "OK", f"Expected: result will be 'OK' but returned: {result}"
+
+
+@scenario(f'../features/simplex.feature', 'Make a deposit by simplex with non exists asset from')
+def test_simplex_deposit_with_non_exists_asset_from():
+    pass
+
+@given(parsers.parse("User try to make a deposit by simplex with asset to {to_asset}, amount {amount},  non exists asset from"))
+def create_payment_with_non_exists_asset_from(to_asset, amount, auth):
+    token = auth(settings.me_tests_email, settings.me_tests_password )
+    result = Simplex().create_payment(token, 'from_asset', to_asset, amount)
+    assert type(result) == dict,  f"Expected: result will be dict type but returned: {result}"
+    assert result['result'] == "AssetDoNotFound", f"Expected: result will be 'AssetDoNotFound' but returned: {result}"
+
+
+@scenario(f'../features/simplex.feature', 'Make a deposit by simplex with non exists asset to')
+def test_simplex_deposit_with_non_exists_asset_to():
+    pass
+
+@given(parsers.parse("User try to make a deposit by simplex with asset from {from_asset}, amount {amount},  non exists asset to"))
+def create_payment_with_non_exists_asset_to(from_asset, amount, auth):
+    token = auth(settings.me_tests_email, settings.me_tests_password )
+    result = Simplex().create_payment(token, from_asset, 'to_asset', amount)
+    assert type(result) == dict,  f"Expected: result will be dict type but returned: {result}"
+    assert result['result'] == "AssetDoNotFound", f"Expected: result will be 'AssetDoNotFound' but returned: {result}"
+
+
+@scenario(f'../features/simplex.feature', 'Make a deposit by simplex with amount more than maximum')
+def test_simplex_deposit_with_amount_more_than_maximum():
+    pass
+
+@given(parsers.parse("User try to make a deposit by simplex with asset to {to_asset}, asset from {from_asset} and amount more than maximum {amount}"))
+def create_payment_with_amount_more_than_maximum(to_asset, from_asset, amount, auth):
+    token = auth(settings.me_tests_email, settings.me_tests_password )
+    result = Simplex().create_payment(token, from_asset, to_asset, amount)
+    assert type(result) == dict,  f"Expected: result will be dict type but returned: {result}"
+    assert result['result'] == "AmountToLarge", f"Expected: result will be 'AmountToLarge' but returned: {result}"
+
+
+@scenario(f'../features/simplex.feature', 'Make a deposit by simplex with amount less than minimum')
+def test_simplex_deposit_with_amount_less_than_minimum():
+    pass
+
+@given(parsers.parse("User try to make a deposit by simplex with asset to {to_asset}, asset from {from_asset} and amount {amount}"))
+def create_payment_with_amount_less_than_minimum(to_asset, from_asset, amount, auth):
+    token = auth(settings.me_tests_email, settings.me_tests_password )
+    result = Simplex().create_payment(token, from_asset, to_asset, amount)
+    assert type(result) == dict,  f"Expected: result will be dict type but returned: {result}"
+    assert result['result'] == "AmountIsSmall", f"Expected: result will be 'AmountIsSmall' but returned: {result}"
