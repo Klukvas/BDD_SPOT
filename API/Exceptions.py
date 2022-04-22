1a9356b6dfed8f5912c785ec9eac653007402d24
Andrey <pavlenkoandrey56@gmail.com>
2022-04-22 18:43:49 +0300

    commet exceptions fabric

--- API/Exceptions.py
+++ API/Exceptions.py
@@ -12,3 +12,17 @@ class MessageNotFound(Exception):
 
 class TooManyMessagesFound(Exception):
     pass
+#
+#
+# class ExceptionsFabric:
+#
+#     def __init__(self, name, add_data) -> None:
+#         self.name = name
+#         self.add_data = add_data
+#
+#     def generate_exceprion(self):
+#         ex = type(self.name, (Exception,), {})
+#         return ex
+#
+#
+# raise ExceptionsFabric('TooMuchRequests').generate_exceprion()('asdasdas')
