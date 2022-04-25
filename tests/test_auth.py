1bf753aa7fe9f978c48d27ec86be8b4afad97305
Andrey <pavlenkoandrey56@gmail.com>
2022-04-26 00:52:44 +0300

    stylish

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -12,6 +12,7 @@ import uuid
 def test_blocker_after_change_password():
     pass
 
+
 @given('User change password', target_fixture='change_password_blocker')
 def change_password_blocker(register):
     email = "test_user_" + str(uuid.uuid4()) + '@mailinator.com'
@@ -24,6 +25,8 @@ def change_password_blocker(register):
         f"{new_password}"
     )
     return {"email": email, "new_password": new_password}
+
+
 @then("User can not make a transfer")
 def transfer_with_blocker(auth, change_password_blocker):
     token = auth(
@@ -39,6 +42,7 @@ def transfer_with_blocker(auth, change_password_blocker):
     )
 
     assert transfer_data['result'] == 'OperationBlocked'
+
 
 @then("User can not make a withdrawal")
 def transfer_with_blocker(auth, change_password_blocker):
@@ -64,7 +68,8 @@ def test_inc_password_blocker():
 
 @given("User input incorrect password for few times", target_fixture='login_with_enc_password')
 def login_with_enc_password(auth, register):
-    for _ in range(0, 4):
+    auth_data = None
+    for _ in range(0, settings.auth_tests_repeat_count):
         auth_data = auth(register['email'], "testpassword2", 'test_blocker')
     return auth_data
 
@@ -90,8 +95,10 @@ def change_password(register):
         password,
         new_password
     )
-    assert type(ch_pass_data) == dict, f"Expected: type == dict\nReturned: {type(ch_pass_data)}\tch_pass_data: {ch_pass_data}"
+    assert type(ch_pass_data) == dict, \
+        f"Expected: type == dict\nReturned: {type(ch_pass_data)}\tch_pass_data: {ch_pass_data}"
     return {"email": email, "new_password": new_password, "old_password": password}
+
 
 @when('User can not logIn by old password')
 def log_in_by_old_password(auth, password_changed):
@@ -104,7 +111,8 @@ def log_in_by_old_password(auth, password_changed):
 @then('User can logIn by new password')
 def log_in_by_new_password(auth, password_changed):
     auth_data = auth(password_changed['email'], password_changed['new_password'])
-    assert type(auth_data) == str, f"Expected that response will be str vut returned: {type(auth_data)}\tauth_data:{auth_data}"
+    assert type(auth_data) == str, \
+        f"Expected that response will be str vut returned: {type(auth_data)}\tauth_data:{auth_data}"
 
 
 @scenario(f'../features/auth.feature', 'LogOut')
@@ -116,7 +124,7 @@ def test_logout():
 def get_token(auth):
     token = auth(settings.template_tests_email, settings.template_tests_password)
     assert type(token) == str, f"Expected: type == str\nReturned: {type(token)}\treg_data:{token}"
-    return { "token": token}
+    return {"token": token}
 
 
 @given('User can interact with any endpoint')
@@ -130,7 +138,8 @@ def log_out(get_token):
     
     logout_data = Auth(None, None).logout(get_token['token'])
     assert type(logout_data) == dict, f"Expected that response will be dict but returned: {logout_data}"
-    assert logout_data['response']['result'] == 'OK', f"Expected that result of response will be eql to OK but returned: {logout_data}"
+    assert logout_data['response']['result'] == 'OK', \
+        f"Expected that result of response will be eql to OK but returned: {logout_data}"
 
 
 @then('User can not interact with endpoint with old token')
@@ -149,7 +158,7 @@ def test_refreshtoken():
 def get_token(auth):
     tokens = auth(settings.template_tests_email, settings.template_tests_password, 'need refresh token')
     assert type(tokens) == list, f"Expected: type == str\nReturned: {type(tokens)}\t tokens: {tokens}"
-    return { "tokens": tokens}
+    return {"tokens": tokens}
 
 
 @given('User can interact with endpoints')
@@ -162,8 +171,11 @@ def check_token(get_token):
 def refresh(get_token):
     new_token = Auth(None, None).refresh(get_token['tokens'][1])
     assert type(new_token) == dict, f"Expected that response will be dict but returned: {new_token}"
-    assert all([k in ['refreshToken', 'token'] for k in list( new_token['response']['data'].keys() )]), f"Expected that response contains next keys: refreshToken, token< but response is: {new_token['response'].keys()}\tnew_token{new_token}"
-    return { "new_tokens": new_token }
+    assert all(
+        [k in ['refreshToken', 'token'] for k in list(new_token['response']['data'].keys())]
+    ), f"""Expected that response contains next keys: refreshToken, token;
+    But response is: {new_token['response'].keys()}\tnew_token{new_token}"""
+    return {"new_tokens": new_token}
 
 
 @then('User can interact with endpoint with new token')
@@ -180,9 +192,11 @@ def test_negative_registration():
 @given(parsers.parse('User try to registration with {email} and {password}. User get {response} with {status_code}'))
 def registration_with_broke_data(email, password, response, status_code):
     negative_response = Auth(email, password).register('negative cases')
-    assert type(negative_response) == dict, f"Expected: type == str\nReturned: {type(negative_response)}\t tokens: {negative_response}"
+    assert type(negative_response) == dict, \
+        f"Expected: type == str\nReturned: {type(negative_response)}\t tokens: {negative_response}"
     assert negative_response['response'] == response, f"Expected: {response}\nReturned: {negative_response['response']}"
-    assert negative_response['status'] == int(status_code),f"Expected: {status_code}\nReturned: {negative_response['status']}"
+    assert negative_response['status'] == int(status_code),\
+        f"Expected: {status_code}\nReturned: {negative_response['status']}"
 
 
 @scenario(f'../features/auth.feature', 'Negative authentification')
@@ -190,12 +204,16 @@ def test_negative_authentification():
     pass
 
 
-@given(parsers.parse("User try to authentification with {email} and {password}. User get {response} with {status_code}"))
+@given(parsers.parse(
+    "User try to authentification with {email} and {password}. User get {response} with {status_code}")
+)
 def authentification_negative(email, password, response, status_code):
     negative_response = Auth(email, password).authenticate('negative cases')
-    assert type(negative_response) == dict, f"Expected: type == str\nReturned: {type(negative_response)}\t tokens: {negative_response}"
+    assert type(negative_response) == dict,\
+        f"Expected: type == str\nReturned: {type(negative_response)}\t tokens: {negative_response}"
     assert negative_response['response'] == response, f"Expected: {response}\nReturned: {negative_response['response']}"
-    assert negative_response['status'] == int(status_code),f"Expected: {status_code}\nReturned: {negative_response['status']}"
+    assert negative_response['status'] == int(status_code),\
+        f"Expected: {status_code}\nReturned: {negative_response['status']}"
 
 
 @scenario(f'../features/auth.feature', 'Negative change password')
@@ -203,8 +221,10 @@ def test_negative_change_password():
     pass
 
 
-@given(parsers.parse("User try to change password from {password_old} to {password_new}. User get {response} with {status_code}"))
-def hange_password_negative(auth, password_old, password_new, response, status_code):
+@given(parsers.parse(
+    "User try to change password from {password_old} to {password_new}. User get {response} with {status_code}")
+)
+def handle_password_negative(auth, password_old, password_new, response, status_code):
     token = auth(settings.auth_tests_email_for_change_password, settings.auth_tests_password_for_change_password)
     if password_old == 'default':
         password_old = settings.auth_tests_password_for_change_password
@@ -212,5 +232,7 @@ def hange_password_negative(auth, password_old, password_new, response, status_c
         settings.auth_tests_email_for_change_password, 
         settings.auth_tests_password_for_change_password
     ).change_password(token, password_old, password_new, "negative cases")
-    assert str(change_password_resp['code']) == status_code, f"Expected status code eql to {status_code} but returned: {change_password_resp}"
-    assert change_password_resp['resp'] == response, f"Expected status code eql to {status_code} but returned: {change_password_resp}"
+    assert str(change_password_resp['code']) == status_code,\
+        f"Expected status code eql to {status_code} but returned: {change_password_resp}"
+    assert change_password_resp['resp'] == response,\
+        f"Expected status code eql to {status_code} but returned: {change_password_resp}"
