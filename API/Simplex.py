6c4362e3482fc11966c68d110cfd6ef346c3d000
Andrey <pavlenkoandrey56@gmail.com>
2022-04-28 21:52:44 +0300

    fix tests of simplex

--- API/Simplex.py
+++ API/Simplex.py
@@ -10,7 +10,7 @@ class Simplex(MainObj):
         super().__init__()
         self.main_url = settings.url_simplex
 
-    def create_payment(self, token: str, _from: str, to: str, amount: int, specific_case) -> dict or int:
+    def create_payment(self, token: str, _from: str, to: str, amount: int, specific_case=False) -> dict or int:
         url = f"{self.main_url}payment"
         payload = {
             "fromCurrency": _from,
